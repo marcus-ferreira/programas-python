dc74deb63707d0727fb7d007053757f46df9f80e
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2020-10-05 01:38:48 -0300

    Included gui

--- rendimentos.py
+++ rendimentos.py
@@ -2,4 +2,4 @@ vp = float(input("Qual o valor da aplicação? "))
 j = float(input("Qual a taxa de juros? (a.m. em %) ")) / 100
 n = int(input("Qual o tempo da aplicação em meses? "))
 vf = float(vp * ((1 + j) ** n))
-print("O valor a ser recebido será de R$ %.2f" % vf)
+print(f"O valor a ser recebido será de R$ {round(vf, 2)}")
